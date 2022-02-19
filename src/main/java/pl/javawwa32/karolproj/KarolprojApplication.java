41f29bbd06113e26e510a409bae0396e5c062f35
KarolSzymaniak <inz.karol.szymaniak@gmail.com>
2022-02-19 13:49:30 +0100

    initial commit

---
+++ src/main/java/pl/javawwa32/karolproj/KarolprojApplication.java
@@ -0,0 +1,13 @@
+package pl.javawwa32.karolproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarolprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarolprojApplication.class, args);
+	}
+
+}
