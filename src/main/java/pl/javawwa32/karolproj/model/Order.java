189aeb7c4aff7bca8b026103bdd4831ce0a66939
lukaszkiraga <lukaszkiraga@gmail.com>
2022-02-27 11:04:00 +0100

    Klasa order - relacje

--- src/main/java/pl/javawwa32/karolproj/model/Order.java
+++ src/main/java/pl/javawwa32/karolproj/model/Order.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -17,15 +18,31 @@ public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    @Column(name = "order_id")
     private Long OrderId;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+
     @Enumerated(EnumType.STRING)
+    @Column(name = "order_status")
     private OrderStatus orderStatus;
+
+    @Column(name = "address")
     private String address;
-    //private  Copy copyId;
-    //private User userId;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.PERSIST)
+    private  Set<MovieCopy> copyId;
+
+    @Column(name = "price")
     private BigDecimal price;
+
+    @Column(name = "return_date")
     private LocalDateTime returnDate;
+
+    @Column(name = "order_date")
     private LocalDateTime orderDate;
 
 }
